0f1c2fe15a5db502511d33854cc2a6c147af02da
Gabriel Salvador <gabs.fsalvador@gmail.com>
2023-08-31 09:12:14 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from './components/Header'
+
+import './global.css'
+import styles from './App.module.css'
+
+export function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
